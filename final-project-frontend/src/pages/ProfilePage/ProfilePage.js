369e677ad46c583614e9f110214f0afc88da7f1c
JanaTartousy <tartousyjana@gmail.com>
2023-05-22 12:06:06 +0300

    profile page

--- final-project-frontend/src/pages/ProfilePage/ProfilePage.js
+++ final-project-frontend/src/pages/ProfilePage/ProfilePage.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import "./ProfilePage.css"
+import profileImage from "../../images/user.webp"
 
 function ProfilePage() {
   return (
-    <div className='profile-wrap-container'>ProfilePage</div>
+    <div className='profile-wrap-container'>
+        <h2 className="profile-title">Profile</h2>
+                    <section className="profile-whole">
+                        <img
+                            src={profileImage}
+                            alt="User Profile"
+                            className="profile-image"
+                        />
+                        <section className="profile-informations">
+                            <p className="profile-info">Username: </p>
+                           
+                                <p className="profile-info">Email: </p>
+                            
+                            
+                        </section>
+                    </section>
+    </div>
   )
 }
 
